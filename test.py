dab91049ff9003d76ac65a4fa693d24ec086ae94
Suvash Joshi <suvash2077@gmail.com>
2021-05-25 17:24:34 +0545

    added web_scarping example

--- test.py
+++ test.py
@@ -1,15 +1,10 @@
-def printBox(symbol, height, width):
-    if len(symbol) != 1:
-        raise Exception("symbol must be 1 character")
-    
-    if (height < 2) or (width < 2):
-        raise Exception("Height or width cannot be less than 2.")
+import webbrowser, sys, pyperclip
 
-    print (symbol * width)
-    for i in range(height-2):
-        print(symbol + (' ' * (width -2)) + symbol)
-    print (symbol * width)
+sys.argv
 
-printBox("*", 5, 6)
-printBox("#", 2, 3)
-printBox("$", 2, 1)
+if len(sys.argv) > 1:
+    twitch_name = sys.argv[1]
+else:
+    twitch_name = pyperclip.paste()
+
+webbrowser.open("https://twitch.tv/"+twitch_name)
