57d932205c841e13bf6b4c2252e22cfb2edc0cef
Suvash Joshi <suvash2077@gmail.com>
2021-05-27 20:08:33 +0545

    Updated mass_name_replacer.py

--- Mass_file_renamer.py
+++ Mass_file_renamer.py
@@ -3,10 +3,10 @@
 import os
 
 path = input("Enter the folder directory: ")
+ext = input("Enter the extension of file you want to rename: ")
 os.chdir(path)
 
 for i, file in enumerate(sorted(os.listdir(path))):
-    if '.mp4' in file:
-        ext = file[len(file)-4:]
+    if ext in file:
         newname = file[:20] + str(i+1) + ext
         os.rename(file, newname)
